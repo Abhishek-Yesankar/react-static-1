fcc5ba5d26b087f02d16289be465c7f0865bbef8
unknown <abhishekyesankar12@gmail.com>
2022-12-15 19:34:32 +0530

    init

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import {motion} from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.div
+	//  initial={{opacity: 0}}
+	//  animate={{opacity: 1}}
+	//  exit={{opacity: 0}}
+	//  transition={{duration: 1, delay: .2}}
+	 >
+      <h2>About us</h2>
+    </motion.div>
+  );
+}
